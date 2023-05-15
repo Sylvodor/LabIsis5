9f5f419994c92c4bea5d46fce202c350b69a4a52
Sylvodor <vital-0p5@mail.ru>
2023-05-15 22:41:23 +0300

    FirstComit

---
+++ IsisCal/ISisCal/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите число a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int b = Convert.ToInt32(Console.ReadLine());
+int res;
+Console.WriteLine("Введите операцию (+-*/):");
+char c = Convert.ToChar(Console.ReadLine());
+if (c == '+')
+{
+    res = a + b;
+    Console.WriteLine("Результат - " + res);
+}
+else if (c == '-')
+{
+    res = a - b;
+    Console.WriteLine("Результат - " + res);
+}
+else if (c == '*')
+{
+    res = a * b;
+    Console.WriteLine("Результат - " + res);
+}
+else if (c == '/')
+{
+    res = a / b;
+    Console.WriteLine("Результат - " + res);
+}
